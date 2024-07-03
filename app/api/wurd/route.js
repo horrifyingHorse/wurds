97db2299dc939fc2c0a792399a397a3bc2cab2e9
Ayaan Khan <excuseplanation@gmail.com>
2024-07-03 23:45:28 +0530

    endpoint return random word list

--- app/api/wurd/route.js
+++ app/api/wurd/route.js
@@ -1,6 +1,10 @@
 import { NextResponse as res } from "next/server"
 
 export async function GET(req) {
-  const wurd = req.query
-  return res.json({'word': wurd})
+  const { searchParams } = new URL(req.url)
+  const quant = Number(searchParams.get('quant'))
+
+  const externalAPIresp = await fetch(`https://random-word-api.vercel.app/api?words=${quant}`)
+
+  return res.json(await externalAPIresp.json())
 }
