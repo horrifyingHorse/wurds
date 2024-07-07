f12083d3e9cfba06f7c8b9f384460a15d2145b50
Ayaan Khan <excuseplanation@gmail.com>
2024-07-07 11:21:33 +0530

    Add simto param

--- app/api/wurd/route.js
+++ app/api/wurd/route.js
@@ -2,9 +2,17 @@ import { NextResponse as res } from "next/server"
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url)
+
   const quant = Number(searchParams.get('quant'))
+  const simto = String(searchParams.get('simto'))
 
-  const externalAPIresp = await fetch(`https://random-word-api.vercel.app/api?words=${quant}`)
+  let externalAPIresp = ''
 
-  return res.json(await externalAPIresp.json())
+  if (quant) {
+    externalAPIresp = await fetch(`https://random-word-api.vercel.app/api?words=${quant}`)
+  } else if (simto) {
+    externalAPIresp = await fetch(`https://api.datamuse.com/words?sp=${simto}`)
+  }
+
+  return res.json(externalAPIresp == '' ? {'error': 1} : await externalAPIresp.json())
 }
